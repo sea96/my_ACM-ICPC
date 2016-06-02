1cb41d69e6b94dd20c9cbfa8eb3464767567b3f3
RunningTime <baohai111@163.com>
2016-06-02 13:29:48 +0800

    Update InputReader.java

--- InputReader.java
+++ InputReader.java
@@ -1,24 +1,21 @@
 public class ReadDemo {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		InputStream inputstream = System.in;
 		OutputStream outputstream = System.out;
+		//Scanner in = new Scanner (new BufferedInputStream (System.in));
 		InputReader in = new InputReader (inputstream);
 		PrintWriter out = new PrintWriter (outputstream);
 		//
 		out.close ();
 	}
-	
 	static class InputReader {
         public BufferedReader reader;
         public StringTokenizer tokenizer;
-
         public InputReader(InputStream stream) {
             reader = new BufferedReader(new InputStreamReader(stream), 32768);
             tokenizer = null;
         }
-
         public String next() {
             while (tokenizer == null || !tokenizer.hasMoreTokens()) {
                 try {
@@ -29,7 +26,6 @@ public class ReadDemo {
             }
             return tokenizer.nextToken();
         }
-
         public int nextInt() {
             return Integer.parseInt(next());
         }
