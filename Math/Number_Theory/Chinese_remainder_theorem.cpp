e044b87b5650fcab2f9ccfe6df14211f21a6ecbb
RunningTime <baohai111@163.com>
2016-07-02 20:06:23 +0800

    中国剩余定理

--- Math/Number_Theory/Chinese_remainder_theorem.cpp
+++ Math/Number_Theory/Chinese_remainder_theorem.cpp
@@ -1,17 +1,21 @@
 /*
-    *中国剩余定理：求解满足：x = 2 (%3), x = 3 (%5), x = 2 (%7)的最小x
-    *解为：x = 2 * 35a + 3 * 21b + 2 * 15c (%105)
-    *k是方程组的个数，b对应(2，3，2)，m对应(3，5，7)
+    *中国剩余定理：求解满足：x = 2(% 3), x = 3(% 5), x = 2(% 7)的最小x
+    *解为：x = 2*35a + 3*21b + 2*15c (%105)
+    *x=a[i](% m[i]), n是方程组的个数，a对应(2，3，2)，m对应(3，5，7)
 */
-int China(int k, int *b, int *m) {
+int China(int k, int *a, int *m) {
     ll M = 1, x, y, ret = 0;
-    for (int i=1; i<=k; ++i) {
+    for (int i=1; i<=n; ++i) {
         M *= m[i];
     }
-    for (int i=1; i<=k; ++i) {
+    for (int i=1; i<=n; ++i) {
         ll w = M / m[i];
         ex_GCD (w, m[i], x, y);
-        ret += multi_mod (multi_mod (x, w, M), b[i], M); //防爆long long
+        ret = (ret + x * w * a[i]) % M;
     }
-    return (ret % M + M) % M;
+    return (ret + M) % M;
 }
+/*
+   *做法：令M=m[i]的乘积，w[i]=M/m[i]，GCD(w[i], m[i])=1，求解w[i]*x+m[i]*y=1
+   *令e[i]=w[i]*x，ans = sum(e[i]*a[i]);
+*/
