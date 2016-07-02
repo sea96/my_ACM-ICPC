4062633eaad3b6a2650c1cbb7a0ecea32478a070
RunningTime <baohai111@163.com>
2016-07-02 18:43:44 +0800

    逆元

--- Math/Number_Theory/inverse.cpp
+++ Math/Number_Theory/inverse.cpp
@@ -1,19 +1,22 @@
-//求逆元，三种方法都要求p为质数
-//求法1：预处理，数组保存，复杂度 O (n)
-void Inv(int n, int p) {
+//求逆元，三种方法都要求mod为质数
+//求法1：预处理，数组保存，复杂度 O(n)
+void Inv(int n, int mod) {
     inv[1] = 1;
     for (int i=2; i<=n; ++i) {
-        inv[i] = 1ll * (p - p / i) * inv[p%i] % p;
+        inv[i] = 1ll * (mod - mod / i) * inv[mod%i] % mod;
     }
 }
-//求法2：复杂度 O (logn)
-int Inv(int a, int p) {
-    return pow_mod (a, p - 2, p);
+//求法2：复杂度 O(logn)
+int Inv(int a, int mod) {
+    return modow_mod (a, mod - 2, mod);
 }
 //求法3：扩展欧几里德求解
-int Inv(int a, int p) {
+int Inv(int a, int mod) {
     int x, y;
-    int d = ex_GCD (a, p, x, y);
-    if (d == 1) return (x % p + p) % p;
-    else    return -1;
+    int d = ex_GCD (a, mod, x, y);
+    if (d == 1) {
+        return (x % mod + mod) % mod;
+    } else {
+        return -1;
+    }
 }
