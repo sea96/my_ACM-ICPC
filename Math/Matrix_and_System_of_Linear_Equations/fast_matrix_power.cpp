0a90e06d445a2d8fdab0899ee1a54f2efb6ed251
RunningTime <baohai111@163.com>
2016-07-10 14:37:17 +0800

    矩阵快速幂

--- Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
+++ Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
@@ -1,13 +1,12 @@
+/*
+	*矩阵快速幂处理线性递推关系f(n)=a1f(n-1)+a2f(n-2)+...+adf(n-d)
+*/
 struct Matrix {
     int row, col;
     int arr[N][N];
     Matrix(int r=0, int c=0) {
-        set_size (r, c);
+        row = r; col = c;
         memset (arr, 0, sizeof (arr));
-    }
-    void set_size(int row, int col) {
-        this->row = row;
-        this->col = col;
     }
     Matrix operator * (const Matrix &B) {
         Matrix ret(row, B.col);
@@ -21,13 +20,13 @@ struct Matrix {
         return ret;
     }
     void init(int n) {
-        set_size (n, n);
+        row = col = n;
         for (int i=0; i<n; ++i) {
             arr[i][i] = 1;
         }
-    }    
+    }
 };
-Matrix operator ^ (Matrix X, int n) {
+Matrix operator ^ (Matrix X, ll n) {
     Matrix ret; ret.init (X.col);
     while (n) {
         if (n & 1) {
@@ -37,4 +36,26 @@ Matrix operator ^ (Matrix X, int n) {
         n >>= 1;
     }
     return ret;
+}
+
+int main() {
+    //input a[i], f[i] (1<=i<=d)
+    if (n <= d) {
+        //f[n];
+    } else {
+        Matrix Fn(d+1, d+1), Fd(d+1, 1);
+        for (int i=0; i<Fn.row-1; ++i) {
+            Fn.arr[i][i+1] = 1;
+        }
+        for (int i=1; i<Fn.col; ++i) {
+            Fn.arr[Fn.row-1][i] = a[d-i+1];
+        }
+        for (int i=0; i<Fd.row; ++i) {
+            Fd.arr[i][0] = f[i];
+        }
+        Fn = Fn ^ (n - d);
+        Fn = Fn * Fd;
+        //f[n] = Fn.arr[d][0];
+    }
+    return 0;
 }
