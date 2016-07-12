960725ea8b04465151b3cfb20b80eba1c36f4ecc
RunningTime <baohai111@163.com>
2016-07-12 14:28:40 +0800

    二维树状数组

---
+++ Data_Structure/2D_BIT.cpp
@@ -0,0 +1,30 @@
+//2D
+struct BIT {
+    int c[105][N][N], n, m;
+    void init(int n, int m) {
+        memset (c, 0, sizeof (c));
+        this->n = n;
+        this->m = m;
+    }
+    //k=a[x][y], z=add
+    void updata(int k, int x, int y, int z) {
+        for (int i=x; i<=n; i+=i&(-i)) {
+            for (int j=y; j<=m; j+=j&(-j)) {
+                c[k][i][j] += z;
+            }
+        }
+    }
+    int query(int k, int x, int y) {
+        int ret = 0;
+        for (int i=x; i; i-=i&(-i)) {
+            for (int j=y; j; j-=j&(-j)) {
+                ret += c[k][i][j];
+            }
+        }
+        return ret;
+    }
+    //count a
+    int count(int x1, int x2, int y1, int y2, int a) {
+        return query (a, x2, y2) - query (a, x2, y1-1) - query (a, x1-1, y2) + query (a, x1-1, y1-1);
+    }
+};
