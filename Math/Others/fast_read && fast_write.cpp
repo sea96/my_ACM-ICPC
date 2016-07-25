10144b86c6a24ab4b096855d4f12ec411537fc1c
RunningTime <baohai111@163.com>
2016-07-25 09:57:50 +0800

    快速读入，输出

--- Math/Others/fast_read && fast_write.cpp
+++ Math/Others/fast_read && fast_write.cpp
@@ -7,7 +7,7 @@ inline void read(int &x) {
     x *= f;
 }
 inline void write(int x) {
-	if (x < 0) putchar ('-'), x = -x;
+    if (x < 0) putchar ('-'), x = -x;
     if (x > 9) write (x / 10);
     putchar (x % 10 + '0');
 }
