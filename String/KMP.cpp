fc15fccc3d14aac4bbaaed9378cc4dbccb18e431
RunningTime <baohai111@163.com>
2016-07-29 19:05:23 +0800

    KMP算法

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -2,24 +2,25 @@
     T[]是文本串，P[]是模式串，lent，lenp是各自的长度
     返回第一个匹配完成在文本串上的位置，不成功返回-1
 */
-void get_fail(char *P, int lenp)  {
-    int i = 0, j = -1;  fail[0] = -1;
-    while (i < lenp)    {
-        if (j == -1 || P[j] == P[i])    {
-            i++;    j++;    fail[i] = j;
+void get_fail(char *P) {
+    int i = 0, j = -1;
+    fail[0] = -1;
+    while (i < lenP) {
+        if (j == -1 || P[j] == P[i]) {
+            i++; j++; fail[i] = j;
         }
-        else    j = fail[j];
+        else j = fail[j];
     }
 }
- 
-int KMP(char *T, char *P)   {
-    int lent = strlen (T), lenp = strlen (P);
-    get_fail (P, lenp);
+
+void KMP(char *T, char *P) {
+    lenT = strlen (T);
+    lenP = strlen (P);
+    get_fail (P);
     int i = 0, j = 0;
-    while (i < lent)  {
+    while (i < lenT) {
         while (j != -1 && T[i] != P[j]) j = fail[j];
         i++; j++;
-        if (j == lenp)  return (i - j + 1);
+        if (j == lenP) return i - j + 1;
     }
-    return -1;
 }
