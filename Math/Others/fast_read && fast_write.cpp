8b25d84f43bffa5164092b6147f3f9b161849600
RunningTime <baohai111@163.com>
2016-08-15 11:06:19 +0800

    快速读入，输出

--- Math/Others/fast_read && fast_write.cpp
+++ Math/Others/fast_read && fast_write.cpp
@@ -1,11 +1,36 @@
 //快速读入，输出
 inline void read(int &x) {
     char ch = getchar ();
-    int f = 1; x = 0;
-    while (ch < '0' || ch > '9') if (ch == '-') f = -1, ch = getchar ();
-    while (ch >= '0' && ch <= '9') x = x * 10 + (ch - '0'), ch = getchar ();
-    x *= f;
+    bool sign = false; x = 0;
+    for (; ch < '0' || ch > '9'; ch=getchar ()) if (ch == '-') sign = true;
+    for (; ch >= '0' && ch <= '9'; ch=getchar ()) x = x * 10 + (ch - '0');
+    if (sign) x = -x;
 }
+
+inline void read(double &x) {
+    char ch = getchar ();
+    bool sign = false; x = 0;
+    for (; ch < '0' || ch > '9'; ch=getchar ()) if (ch == '-') sign = true;
+    for (; ch >= '0' && ch <= '9'; ch=getchar ()) x = x * 10 + (ch - '0');
+    if (ch == '.') {
+        double tmp = 1;
+        ch = getchar ();
+        for (; ch >= '0' && ch <= '9'; ch=getchar ()) tmp /= 10, x += tmp * (ch - '0');
+    }
+    if (sign) x = -x;
+}
+
+inline bool blank(char ch) {
+    return ch==' ' || ch=='\n' || ch=='\r' || ch=='\t';
+}
+
+inline void read(char *s) {
+    char ch = getchar ();
+    for (; blank (ch); ch=getchar ());
+    for (; !blank (ch); ch=getchar ()) *s++ = ch;
+    *s = 0;
+}
+
 inline void write(int x) {
     if (x < 0) putchar ('-'), x = -x;
     if (x > 9) write (x / 10);
