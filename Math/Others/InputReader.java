e6e1bb00bb22b922f9825c85beff8ae761ca3843
RunningTime <baohai111@163.com>
2016-08-20 09:33:33 +0800

    Java快速读入输出

--- Math/Others/InputReader.java
+++ Math/Others/InputReader.java
@@ -3,36 +3,36 @@ import java.util.*;
 import java.math.*;
 
 public class ReadDemo  {
-	public static void main(String[] args) {
-		//Scanner in = new Scanner(new BufferedInputStream(System.in));
-		
-		InputStream inputstream = System.in;
-		OutputStream outputstream = System.out;
-		
-		InputReader in = new InputReader(inputstream);
-		PrintWriter out = new PrintWriter(outputstream);
-		
-		Task solver = new Task();
-		solver.solve(1, in, out);
-		
-		out.close ();
-	}
-	
-	static class Task {
-		public void solve(int testNumber, InputReader in, PrintWriter out) {
-			// solve here
-		}
-	}
-	
-	static class InputReader {
+    public static void main(String[] args) {
+        //Scanner in = new Scanner(new BufferedInputStream(System.in));
+
+        InputStream inputstream = System.in;
+        OutputStream outputstream = System.out;
+
+        InputReader in = new InputReader(inputstream);
+        PrintWriter out = new PrintWriter(outputstream);
+
+        Task solver = new Task();
+        solver.solve(1, in, out);
+
+        out.close ();
+    }
+
+    static class Task {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            // solve here
+        }
+    }
+
+    static class InputReader {
         public BufferedReader reader;
         public StringTokenizer tokenizer;
-        
+
         public InputReader(InputStream stream) {
             reader = new BufferedReader(new InputStreamReader(stream), 32768);
             tokenizer = null;
         }
-        
+
         public String next() {
             while (tokenizer == null || !tokenizer.hasMoreTokens()) {
                 try {
@@ -43,13 +43,13 @@ public class ReadDemo  {
             }
             return tokenizer.nextToken();
         }
-        
+
         public int nextInt() {
             return Integer.parseInt(next());
         }
-        
+
         public BigInteger nextBigInteger() {
-        	return new BigInteger(next());
+            return new BigInteger(next());
         }
     }
 }
