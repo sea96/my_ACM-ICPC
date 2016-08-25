9a93d9cf5ac2c9fa23bfda1a6f55d13f0aae209e
RunningTime <baohai111@163.com>
2016-08-25 08:54:53 +0800

    2-SAT

---
+++ Graph/2-SAT.cpp
@@ -0,0 +1,44 @@
+/*
+    *2-SAT，暴力判断，注意从0开始
+*/
+struct Two_SAT {
+    int n;
+    std::vector<int> edge[N<<1];
+    bool mark[N<<1];
+    int sta[N<<1], top;
+    
+    void init(int n) {
+        this->n = n;
+        for (int i=0; i<=n*2; ++i) edge[i].clear ();
+        memset (mark, false, sizeof (mark));
+    }
+    //x+valx = true or y+valy = true
+    void add_edge(int x, int valx, int y, int valy) {
+        x = x * 2 + valx;
+        y = y * 2 + valy;
+        edge[x^1].push_back (y);
+        edge[y^1].push_back (x);
+    }
+    bool DFS(int x) {
+        if (mark[x^1]) return false;
+        if (mark[x]) return true;
+        mark[x] = true;
+        sta[top++] = x;
+        for (int y: edge[x]) {
+            if (!DFS (y)) return false;
+        }
+        return true;
+    }
+    bool judge() {
+        for (int i=0; i<n*2; i+=2) {
+            if (!mark[i] && !mark[i+1]) {
+                top = 0;
+                if (!DFS (i)) {
+                    while (top > 0) mark[sta[--top]] = false;
+                    if (!DFS (i+1)) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
