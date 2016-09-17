9dbd729ac9bc4d23ea1880489161ed220cf0595f
RunningTime <baohai111@163.com>
2016-09-17 21:32:28 +0800

    最短路

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,20 +1,42 @@
-void Dijkstra(int s) {
-    memset (vis, false, sizeof (vis));
-    memset (d, INF, sizeof (d));
-    d[s] = 0;
-    std::priority_queue<Edge> pque;
-    pque.push (Edge (s, d[s]));
-    while (!pque.empty ()) {
-        int u = pque.top ().v;
-        pque.pop ();
-        if (vis[u]) continue;
-        vis[u] = true;
-        for (int i=head[u]; ~i; i=edge[i].nex) {
-            Edge &e = edge[i];
-            if (!vis[e.v] && d[e.v] > d[u] + e.w) {
-                d[e.v] = d[u] + e.w;
-                pque.push (Edge (e.v, d[e.v]));
+struct Dijkstra {
+    struct Edge {
+        int u, v, w;
+    };
+    int n, m;
+    vector<Edge> edges;
+    vector<int> id[N];
+    int d[N], p[N];
+    bool done[N];
+
+    void init(int n) {
+        this->n = n;
+        edges.clear();
+        for (int i=1; i<=n; ++i) id[i].clear();
+    }
+    void add_edge(int u, int v, int w) {
+        edges.push_back((Edge){u, v, w});
+        m = edges.size();
+        id[u].push_back(m-1);
+    }
+    void dijkstra(int s) {
+        memset(d, INF, sizeof(d));
+        memset(done, false, sizeof(done));
+        d[s] = 0;
+        priority_queue<pair<int, int>> pque;
+        pque.push(make_pair(-d[s], s));
+        while (!pque.empty()) {
+            int u = pque.top().second;
+            pque.pop();
+            if (done[u]) continue;
+            done[u] = true;
+            for (int i: id[u]) {
+                Edge &e = edges[i];
+                if (d[e.v] > d[u] + e.w) {
+                    d[e.v] = d[u] + e.w;
+                    p[e.v] = i;
+                    pque.push(make_pair(-d[e.v], e.v));
+                }
             }
         }
     }
-}
+};
