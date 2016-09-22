8459ec997d5ef223355fffb49da4a5bfade1cb15
RunningTime <baohai111@163.com>
2016-09-22 15:13:41 +0800

    牛顿迭代法

--- Math/Numerical_Integration/Newton's method.java
+++ Math/Numerical_Integration/Newton's method.java
@@ -1,15 +1,15 @@
 public void solve(int testNumber, InputReader in, PrintWriter out) {
-      //求x=sqrt(n)，小数点后保留k位
-			BigDecimal n = in.nextBigDecimal();
-			int k = in.nextInt();
-			
-			BigDecimal x = BigDecimal.valueOf(Math.sqrt(n.doubleValue()));
-			
-      //Newton's method: x(n+1)=x(n)-f(x(n))/f'(x(n))
-      for (int i=0; i<10; ++i) {
-      	x = x.add(n.divide(x, k+5, RoundingMode.HALF_UP));
-      	x = x.divide(BigDecimal.valueOf(2), k+5, RoundingMode.HALF_UP);
-      }
-      x = x.setScale(k, BigDecimal.ROUND_DOWN);
-      out.println(x);
+	//求x=sqrt(n)，小数点后保留k位
+	BigDecimal n = in.nextBigDecimal();
+	int k = in.nextInt();
+	
+	BigDecimal x = BigDecimal.valueOf(Math.sqrt(n.doubleValue()));
+	
+	//Newton's method: x(n+1)=x(n)-f(x(n))/f'(x(n))
+	for (int i=0; i<10; ++i) {
+	x = x.add(n.divide(x, k+5, RoundingMode.HALF_UP));
+	x = x.divide(BigDecimal.valueOf(2), k+5, RoundingMode.HALF_UP);
+	}
+	x = x.setScale(k, BigDecimal.ROUND_DOWN);
+	out.println(x);
 }
