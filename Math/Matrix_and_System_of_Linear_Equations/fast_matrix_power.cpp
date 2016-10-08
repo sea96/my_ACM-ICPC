95926586e871e01f6543327a8547f250ee3a8688
RunningTime <baohai111@163.com>
2016-10-08 15:59:27 +0800

    矩阵快速幂

--- Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
+++ Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
@@ -1,40 +1,25 @@
-int mod;  //全局变量，矩阵乘法取模
 /*
     *矩阵快速幂处理线性递推关系f(n)=a1f(n-1)+a2f(n-2)+...+adf(n-d)
 */
-struct Matrix {
-    int row, col;
-    int arr[N][N];
-    Matrix(int r=0, int c=0) {
-        row = r; col = c;
-        memset (arr, 0, sizeof (arr));
-    }
-    Matrix operator * (const Matrix &B) {
-        Matrix ret(row, B.col);
-        for (int i=0; i<row; ++i) {
-            for (int j=0; j<B.col; ++j) {
-                for (int k=0; k<col; ++k) {
-                    ret.arr[i][j] = (ret.arr[i][j] + (ll) arr[i][k] * B.arr[k][j]) % mod;
-                }
-            }
-        }
-        return ret;
-    }
-    void unit(int n) {
-        row = col = n;
-        for (int i=0; i<n; ++i) {
-            arr[i][i] = 1;
-        }
-    }
-};
-Matrix operator ^ (Matrix X, int n) {
-    Matrix ret; ret.unit (X.col);
-    while (n) {
-        if (n & 1) {
-            ret = ret * X;
-        }
-        X = X * X;
-        n >>= 1;
+typedef vector<int> Vec;  //矩阵的一行用vector表示
+typedef vector<Vec> Mat;  //矩阵用多行vector表示
+
+//Mat A(row, Vec(col)); 行长度：A.size(), 列长度：A[0].size()。
+Mat matrix_mul(const Mat &A, const Mat &B) {
+    Mat ret(A.size(), Vec(B[0].size()));
+    for (int i=0; i<A.size(); ++i)
+        for (int j=0; j<B[0].size(); ++j)
+            for (int k=0; k<A[0].size(); ++k)
+                ret[i][j] = (ret[i][j] + (ll)A[i][k]*B[k][j]%MOD) % MOD;
+    return ret;
+}
+
+Mat matrix_pow(Mat X, int n) {
+    Mat ret(X.size(), Vec(X.size()));
+    for (int i=0; i<X.size(); ++i) ret[i][i] = 1;
+    for (; n; n>>=1) {
+        if (n & 1) ret = matrix_mul(ret, X);
+        X = matrix_mul(X, X);
     }
     return ret;
 }
@@ -44,19 +29,19 @@ int main() {
     if (n <= d) {
         //f[n];
     } else {
-        Matrix Fn(d+1, d+1), Fd(d+1, 1);
-        for (int i=0; i<Fn.row-1; ++i) {
-            Fn.arr[i][i+1] = 1;
-        }
-        for (int i=1; i<Fn.col; ++i) {
-            Fn.arr[Fn.row-1][i] = a[d-i+1];
-        }
-        for (int i=0; i<Fd.row; ++i) {
-            Fd.arr[i][0] = f[i];
-        }
-        Fn = Fn ^ (n - d);
-        Fn = Fn * Fd;
-        //f[n] = Fn.arr[d][0];
+        Mat Fn(d+1, Vec(d+1)), Fd(d+1, Vec(1));
+		for (int i=0; i<Fn.size()-1; ++i) {
+			Fn[i][i+1] = 1;
+		}
+		for (int i=1; i<Fn[0].size(); ++i) {
+			Fn[Fn.size()-1][i] = a[d-i+1];
+		}
+		for (int i=0; i<Fd.size(); ++i) {
+			Fd[i][0] = f[i];
+		}
+		Fn = matrix_pow(Fn, n - d);
+		Fn = matrix_mul(Fn, Fd);
+        //f[n] = Fn[d][0];
     }
     return 0;
 }
