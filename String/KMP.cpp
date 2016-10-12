52107f36f603ffbe78155aea046a7898d6198e4e
RunningTime <baohai111@163.com>
2016-10-12 21:08:09 +0800

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -2,10 +2,10 @@
     T[]是文本串，P[]是模式串，lent，lenp是各自的长度
     返回第一个匹配完成在文本串上的位置，不成功返回-1
 */
-void get_fail(char *P) {
+void get_fail(char *P, int lenp) {
     int i = 0, j = -1;
     fail[0] = -1;
-    while (i < lenP) {
+    while (i < lenp) {
         if (j == -1 || P[j] == P[i]) {
             i++; j++; fail[i] = j;
         }
@@ -14,13 +14,15 @@ void get_fail(char *P) {
 }
 
 int KMP(char *T, char *P) {
-    lenT = strlen (T);
-    lenP = strlen (P);
-    get_fail (P);
+    int lent = strlen(T);
+    int lenp = strlen(P);
+    get_fail(P, lenp);
     int i = 0, j = 0;
-    while (i < lenT) {
+    while (i < lent) {
         while (j != -1 && T[i] != P[j]) j = fail[j];
         i++; j++;
-        if (j == lenP) return i - j + 1;
+        if (j == lenp) return i - j + 1;
+        //统计出现次数：ans++; j = 0（不重复）/fail[j]（可重复）
     }
+    return -1;
 }
