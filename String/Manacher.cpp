4a073a7c4040490d383aa11ba330096b7705b237
RunningTime <baohai111@163.com>
2016-10-12 22:12:00 +0800

    Update Manacher.cpp

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,6 +1,6 @@
 /*
-    *最长回文串算法，O(N)
-*/
+ *最长回文串算法，O(N)
+ */
 int Manacher(char *s) {
     int n = strlen (s);
     //"abcde" -> "$#a#b#c#d#e#"
@@ -12,21 +12,14 @@ int Manacher(char *s) {
     n = n * 2 + 2;
     int id = 0; p[0] = 1;
     for (int i=2; i<n; ++i) {
-        if (id + p[id] > i) {
-            p[i] = std::min (p[2*id-i], id + p[id] - i);
-        } else {
-            p[i] = 1;
-        }
-        while (s[i-p[i]] == s[i+p[i]]) {
-            p[i]++;
-        }
-        if (id + p[id] < i + p[i]) {
-        	id = i;
-        }
+        if (id + p[id] > i) p[i] = min(p[2*id-i], id + p[id] - i);
+        else p[i] = 1;
+        while (s[i-p[i]] == s[i+p[i]]) p[i]++;
+        if (id + p[id] < i + p[i]) id = i;
     }
     int ret = 0;
     for (int i=0; i<n-1; ++i) {
-        ret = std::max (ret, p[i] - 1);
+        ret = max(ret, p[i] - 1);
     }
     return ret;
 }
