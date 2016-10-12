2a083320a97259e43e5ba03045582fc5071366fd
RunningTime <baohai111@163.com>
2016-10-12 22:08:23 +0800

    Update min_representation.cpp

--- String/min_representation.cpp
+++ String/min_representation.cpp
@@ -1,19 +1,19 @@
 int min_representation(char *s, int n) {
-	for (int i=0; i<n; ++i) {
+    for (int i=0; i<n; ++i) {
         s[i+n] = s[i];
     }
     s[n*2] = '\0';
 
-	int i = 0, j = 1, k = 0;
+    int i = 0, j = 1, k = 0;
     while(i < n && j < n) {
-		k = 0;
-		while(k < n && s[i+k] == s[j+k]) k++;
-		if(k >= n) break;
-		if(s[i+k] > s[j+k]) {
+        k = 0;
+        while(k < n && s[i+k] == s[j+k]) k++;
+        if(k >= n) break;
+        if(s[i+k] > s[j+k]) {
             i = std:: max (i + k + 1, j + 1);
         } else {
             j = std:: max (i + 1, j + k + 1);
         }
-	}
-	return std::min(i ,j);
+    }
+    return std::min(i ,j);
 }
