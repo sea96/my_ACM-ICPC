fd98d8f6ae6459738d9e9f21bbf63a3033e5b0be
RunningTime <baohai111@163.com>
2017-01-13 14:54:46 +0800

    Update CRT.cpp

--- Math/Number_Theory/CRT.cpp
+++ Math/Number_Theory/CRT.cpp
@@ -1,5 +1,8 @@
 /*
  *中国剩余定理（Chinese_remainder_theorem）：最小的x，x=a[i](mod m[i]) (1<=i<=n)
+ *做法1（互质）：令M=m[i]的乘积，w[i]=M/m[i]，GCD(w[i], m[i])=1，求解Inv(w[i])（w[i]*x+m[i]*y=1）
+ *则ans = sigma(a[i]*w[i]*x);
+ *做法2（不互质）：每次联立两个方程组合并得到新的方程组，重复n-1次
  */
 ll CRT(ll *a, ll *m, int n) {
     ll M = 1, x, y, z, ret = 0;
@@ -11,10 +14,15 @@ ll CRT(ll *a, ll *m, int n) {
     }
     return (ret + M) % M;
 }
-/*
- *做法：令M=m[i]的乘积，w[i]=M/m[i]，GCD(w[i], m[i])=1，求解Inv(w[i])（w[i]*x+m[i]*y=1）
- *ans = sigma(a[i]*w[i]*x);
- *例子：求解满足：x = 2(% 3), x = 3(% 5), x = 2(% 7)的最小x
- *a对应(2，3，2)，m对应(3，5，7)
- *解为：x = 2*35a + 3*21b + 2*15c (%105)
- */
+ll ex_CRT(ll *a, ll *m, int n) {
+    ll lcm = m[0], r = a[0], x, y, d;
+    for (int i=1; i<n; ++i) {
+        ex_GCD(lcm, m[i], x, y, d);
+        if ((a[i]-r) % d) return -1;
+        x = (a[i]-r) / d * x % (m[i]/d);
+        r += x * lcm;
+        lcm = lcm / d * m[i];
+        r %= lcm;
+    }
+    return (r + lcm) % lcm;
+}
