0fc919ecd1412519c69c4ba7a5dc0c77f943db1c
RunningTime <baohai111@163.com>
2017-01-14 15:33:52 +0800

    Update Hash.cpp

--- String/Hash.cpp
+++ String/Hash.cpp
@@ -1,12 +1,11 @@
-void get_hash(char *s) {
-    int len = strlen(s);
-    H[len] = 0;
+const int BASE = 233;
+unsigned long long Hash[N], pw[N];
+void calcHash() {
+    pw[0] = 1;
+    for (int i=1; i<len; ++i) pw[i] = pw[i-1] * BASE;
+    Hash[len] = 0;
     for (int i=len-1; i>=0; --i) {
-        H[i] = H[i+1] * x + (s[i] - 'a');
+        Hash[i] = Hash[i+1]*BASE+(s[i]-'a');
     }
-    xp[0] = 1;
-    for (int i=1; i<len; ++i) {
-        xp[i] = xp[i-1] * x;
-    }
-    //hash[i, i+L) = H[i] - H[i+L] * xp[L];
+    //Hash[i, i+L) = H[i] - H[i+L]*pw[L];
 }
