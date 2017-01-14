c8144a4f502307dbe44d7135b4df6c67c410a3c3
RunningTime <baohai111@163.com>
2017-01-14 16:24:48 +0800

    Update Hash.cpp

--- String/Hash.cpp
+++ String/Hash.cpp
@@ -1,10 +1,10 @@
 const int BASE = 233;
-unsigned long long Hash[N], pw[N];
+unsigned long long _hash[N], pw[N];
 void calcHash() {
     pw[0] = 1;
     for (int i=1; i<len; ++i) pw[i] = pw[i-1] * BASE;
-    Hash[len] = 0;
+    _hash[len] = 0;
     for (int i=len-1; i>=0; --i)
-        Hash[i] = Hash[i+1]*BASE+(s[i]-'a');
-    //Hash[i, i+L) = H[i] - H[i+L]*pw[L];
+        _hash[i] = _hash[i+1]*BASE+(s[i]-'a');
+    //_hash[i, i+L) = _hash[i] - _hash[i+L]*pw[L];
 }
