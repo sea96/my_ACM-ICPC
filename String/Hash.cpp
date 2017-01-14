fed038f0286f0bf80dc761c137d0dc7904f7ba7c
RunningTime <baohai111@163.com>
2017-01-14 15:34:26 +0800

    Update Hash.cpp

--- String/Hash.cpp
+++ String/Hash.cpp
@@ -4,8 +4,7 @@ void calcHash() {
     pw[0] = 1;
     for (int i=1; i<len; ++i) pw[i] = pw[i-1] * BASE;
     Hash[len] = 0;
-    for (int i=len-1; i>=0; --i) {
+    for (int i=len-1; i>=0; --i)
         Hash[i] = Hash[i+1]*BASE+(s[i]-'a');
-    }
     //Hash[i, i+L) = H[i] - H[i+L]*pw[L];
 }
