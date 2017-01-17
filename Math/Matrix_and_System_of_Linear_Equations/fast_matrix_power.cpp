be38d5ad72b16827ffd78018f2da19f9f9293973
RunningTime <baohai111@163.com>
2017-01-17 11:27:56 +0800

    Update fast_matrix_power.cpp

--- Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
+++ Math/Matrix_and_System_of_Linear_Equations/fast_matrix_power.cpp
@@ -1,7 +1,7 @@
 /*
- *矩阵快速幂处理线性递推关系f(n)=a1f(n-1)+a2f(n-2)+...+adf(n-d)
+ *矩阵快速幂处理线性递推关系：f(n)=a1f(n-1)+a2f(n-2)+...+adf(n-d)+c（C是常数）
  */
-typedef vector<int> Vec;  //矩阵的一行用vector表示
+typedef vector<ll> Vec;  //矩阵的一行用vector表示
 typedef vector<Vec> Mat;  //矩阵用多行vector表示
 
 //Mat A(row, Vec(col)); 行长度：A.size(), 列长度：A[0].size()。
@@ -10,7 +10,7 @@ Mat matrix_mul(const Mat &A, const Mat &B) {
     for (int i=0; i<A.size(); ++i)
         for (int j=0; j<A[0].size(); ++j) if (A[i][j])
             for (int k=0; k<B[0].size(); ++k) if (B[j][k])
-                add_mod(ret[i][k], A[i][j]*B[j][k]%MOD);
+                add_mod(ret[i][k], A[i][j]*B[j][k]%mod);
     return ret;
 }
 
@@ -25,23 +25,14 @@ Mat matrix_pow(Mat X, int n) {
 }
 
 int main() {
-    //input a[i], f[i] (1<=i<=d)
-    if (n <= d) {
-        //f[n];
-    } else {
-        Mat Fn(d+1, Vec(d+1)), Fd(d+1, Vec(1));
-        for (int i=0; i<Fn.size()-1; ++i) {
-            Fn[i][i+1] = 1;
-        }
-        for (int i=1; i<Fn[0].size(); ++i) {
-            Fn[Fn.size()-1][i] = a[d-i+1];
-        }
-        for (int i=0; i<Fd.size(); ++i) {
-            Fd[i][0] = f[i];
-        }
-        Fn = matrix_pow(Fn, n - d);
-        Fn = matrix_mul(Fn, Fd);
-        //f[n] = Fn[d][0];
-    }
+    //input a[i], f[i] (1<=i<=d), 
+    Mat Fd(1, Vec(d+1));
+    Mat X(d+1, Vec(d+1));
+    for (int i=0; i<d; ++i) Fd[0][i] = f[d-i];
+    for (int i=0; i<d; ++i) X[i][0] = a[i+1];
+    for (int i=0; i<d-1; ++i) X[i][i+1] = 1;
+    X[d][0] = c; X[d][d] = 1;
+    Mat Fn = matrix_mul(Fd, matrix_pow(X, n-d));
+    //f[n] = Fn[0][0];
     return 0;
 }
