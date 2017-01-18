b34282e206d13ee6a5043cc61bfa92357d59974d
RunningTime <baohai111@163.com>
2017-01-18 09:13:24 +0800

    Update inverse.cpp

--- Math/Number_Theory/inverse.cpp
+++ Math/Number_Theory/inverse.cpp
@@ -1,14 +1,14 @@
-//求逆元，三种方法都要求mod为质数
-//求法1：预处理，数组保存，复杂度 O(n)
+//求逆元，第二种要求mod为质数
+//求法1：预处理，数组保存，复杂度O(n)
 void Inv(int n, int mod) {
     inv[1] = 1;
     for (int i=2; i<=n; ++i) {
         inv[i] = 1ll * (mod - mod / i) * inv[mod%i] % mod;
     }
 }
-//求法2：复杂度 O(logn)
+//求法2：复杂度O(logn)
 int Inv(int a, int mod) {
-    return pow_mod (a, mod - 2, mod);
+    return pow_mod(a, mod - 2, mod);
 }
 //求法3：扩展欧几里德求解
 int Inv(int a, int mod) {
