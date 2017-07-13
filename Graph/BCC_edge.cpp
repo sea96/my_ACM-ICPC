7f8b297489cf4b1768ed9101781bb02aaaeeb842
Harry <sea96@users.noreply.github.com>
2017-07-13 14:18:40 +0800

    Update BCC_edge.cpp

--- Graph/BCC_edge.cpp
+++ Graph/BCC_edge.cpp
@@ -1,24 +1,39 @@
-void Tarjan(int u, int fa) {
-    low[u] = dfn[u] = ++dfs_clock;
+struct Edge {
+    int u, v, nex;
+}edges[M<<1];
+int head[N], etot;
+int dfn[N], dfs_clock;
+int bcc_no[N], bcc_cnt;
+int sta[N], top;
+bool vis[M<<1];
+
+int Tarjan(int u) {
+    int lowu = dfn[u] = ++dfs_clock;
     sta[top++] = u;
     for (int i=head[u]; ~i; i=edges[i].nex) {
-        int v = edges[i].v;
-        if (v == fa) continue;
-        if (!dfn[v]) Tarjan(v, u);
-        low[u] = min(low[u], low[v]);  //合并写法
-    }
-    if (low[u] == dfn[u]) {  //u是双连通分量的第一个点
-        for (; ;) {
-            int x = sta[--top];
-            low[x] = low[u];  //区分不同的双连通分量
-            //low[u] != low[v] 的边就是桥
-            if (x == u) break;
+		int v = edges[i].v;
+        if (!dfn[v]) {
+        	vis[i] = vis[i^1] = true;
+            lowu = min(lowu, Tarjan(v));
+        } else if (!vis[i]) {  // 支持有重边的图
+	        vis[i] = vis[i^1] = true;
+        	lowu = min(lowu, dfn[v]);
         }
     }
+    if (lowu == dfn[u]) {  //u是边双连通分量的第一个点
+        ++bcc_cnt;
+        int x;
+        do {
+            x = sta[--top];
+			bcc_no[x] = bcc_cnt;
+        } while (x != u);
+    }
+    return lowu;
 }
 
-void find_bcc() {
-    memset (dfn, 0, sizeof(dfn));
-    dfs_clock = top = 0;
-    for (int i=1; i<=n; ++i) Tarjan(1, 0);
+void findBCC() {
+    memset(dfn, 0, sizeof(dfn));
+    memset(bcc_no, 0, sizeof(bcc_no));
+    dfs_clock = bcc_cnt = top = 0;
+    for (int i=1; i<=n; ++i) if (!dfn[i]) Tarjan(i);
 }
