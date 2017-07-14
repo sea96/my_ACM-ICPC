df9024fcbb59c8bb4093c5a0c2e6e8174fe1794b
Harry <sea96@users.noreply.github.com>
2017-07-14 15:33:58 +0800

    Update Guass_Jordan_elimination.cpp

--- Math/Matrix_and_System_of_Linear_Equations/Guass_Jordan_elimination.cpp
+++ Math/Matrix_and_System_of_Linear_Equations/Guass_Jordan_elimination.cpp
@@ -1,20 +1,14 @@
-void Guass_Jordan(Matrix A, int n) {
+void GuassJordan(Matrix A, int n) {
     int i, j, k, r;
     for (i=0; i<n; ++i) {
         r = i;
         for (j=i+1; j<n; ++j) {
-            if (fabs (A[j][i]) > fabs (A[r][i])) {
-                r = j;
-            }
+            if (fabs(A[j][i]) > fabs(A[r][i])) r = j;
         }
         //放弃这一行，直接处理下一行
-        if (fabs (A[r][i]) < EPS) {
-            continue;
-        }
+        if (fabs (A[r][i]) < EPS) continue;
         if (r != i) {
-            for (j=0; j<=n; ++j) {
-                std::swap (A[r][j], A[i][j]);
-            }
+            for (j=0; j<=n; ++j) swap(A[r][j], A[i][j]);
         }
         //与除了第i行外的其他行进行消元
         for (k=0; k<n; ++k) {
