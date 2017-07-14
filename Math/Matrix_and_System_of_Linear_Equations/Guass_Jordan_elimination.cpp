571a31a153c4766a848c3d04b45ea18a9bb943b5
Harry <sea96@users.noreply.github.com>
2017-07-14 15:35:00 +0800

    Update Guass_Jordan_elimination.cpp

--- Math/Matrix_and_System_of_Linear_Equations/Guass_Jordan_elimination.cpp
+++ Math/Matrix_and_System_of_Linear_Equations/Guass_Jordan_elimination.cpp
@@ -2,9 +2,7 @@ void GuassJordan(Matrix A, int n) {
     int i, j, k, r;
     for (i=0; i<n; ++i) {
         r = i;
-        for (j=i+1; j<n; ++j) {
-            if (fabs(A[j][i]) > fabs(A[r][i])) r = j;
-        }
+        for (j=i+1; j<n; ++j) if (fabs(A[j][i]) > fabs(A[r][i])) r = j;
         //放弃这一行，直接处理下一行
         if (fabs (A[r][i]) < EPS) continue;
         if (r != i) {
@@ -12,11 +10,8 @@ void GuassJordan(Matrix A, int n) {
         }
         //与除了第i行外的其他行进行消元
         for (k=0; k<n; ++k) {
-            if (k != i) {
-                for (j=n; j>=i; --j) {
-                    A[k][j] -= A[k][i] / A[i][i] * A[i][j];
-                }
-            }
+            if (k == i) continue;
+            for (j=n; j>=i; --j) A[k][j] -= A[k][i] / A[i][i] * A[i][j];
         }
     }
 }
