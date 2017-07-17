55770c3c682e262f12a12b3b907232cfab577da2
Harry <sea96@users.noreply.github.com>
2017-07-17 14:00:54 +0800

    Update Hash.cpp

--- String/Hash.cpp
+++ String/Hash.cpp
@@ -1,10 +1,14 @@
-const int BASE = 233;
-unsigned long long _hash[N], pw[N];
-void calcHash() {
+typedef unsigned long long ull;
+ull pw[N];
+ull H[N];
+ull seed = 233;
+// ull容易被卡(CF822E)，但换MOD速度慢
+void initHash(char *s, int len) {
+    H[len] = 0;
+    for (int i=len-1; i>=0; --i) {
+        H[i] = H[i+1] * seed + s[i];
+    }
     pw[0] = 1;
-    for (int i=1; i<len; ++i) pw[i] = pw[i-1] * BASE;
-    _hash[len] = 0;
-    for (int i=len-1; i>=0; --i)
-        _hash[i] = _hash[i+1]*BASE+(s[i]-'a');
-    //_hash[i, i+L) = _hash[i] - _hash[i+L]*pw[L];
+    for (int i=1; i<len; ++i) pw[i] = pw[i-1] * seed;
+    //hash[i, i+L) = H[i] - H[i+L] * pw[L];
 }
