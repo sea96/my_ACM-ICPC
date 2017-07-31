0ebc4765cb76835367666400d48b18f5723307e6
Harry <sea96@users.noreply.github.com>
2017-07-31 14:06:28 +0800

    Update SCC.cpp

--- Graph/SCC.cpp
+++ Graph/SCC.cpp
@@ -1,12 +1,9 @@
-/*
-    *强连通分量，时间复杂度O(V+E) 
-*/
+// 强连通分量，时间复杂度O(V+E)
 int dfn[N], scc_no[N];
-std::vector<int> scc[N];
-int sta[N];
-int dfs_clock, top, scc_cnt;
-
-std::vector<int> edge[N];
+int sta[N], top;
+int dfs_clock, scc_cnt;
+vector<int> scc[N];
+vector<int> edge[N];
 int n, m;
 
 int Tarjan(int u) {
@@ -15,28 +12,28 @@ int Tarjan(int u) {
     for (int v: edge[u]) {
         if (!dfn[v]) {
             int lowv = Tarjan (v);
-            lowu = std::min (lowu, lowv);
+            lowu = min(lowu, lowv);
         } else if (!scc_no[v]) {
-            lowu = std::min (lowu, dfn[v]);
+            lowu = min(lowu, dfn[v]);
         }
     }
     if (lowu == dfn[u]) {
         scc[++scc_cnt].clear ();
         for (; ;) {
             int x = sta[--top];
-            scc[scc_cnt].push_back (x);
             scc_no[x] = scc_cnt;
+            scc[scc_cnt].push_back(x);
             if (x == u) break;
         }
     }
     return lowu;
 }
 
-void find_scc() {
-    memset (dfn, 0, sizeof (dfn));
-    memset (scc_no, 0, sizeof (scc_no));
+void findSCC() {
+    memset(dfn, 0, sizeof(dfn));
+    memset(scc_no, 0, sizeof(scc_no));
     dfs_clock = top = scc_cnt = 0;
     for (int i=1; i<=n; ++i) {
-        if (!dfn[i]) Tarjan (i);
+        if (!dfn[i]) Tarjan(i);
     }
 }
